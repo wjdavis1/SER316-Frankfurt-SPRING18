7890f5e6eea0044eb34b8a37dba1b0abe7235451
jsabbath <jsabbath@asu.edu>
2018-02-03 13:49:54 -0700

    updated p2pSplash resource address

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -154,7 +154,7 @@ public class App {
 	private void showSplash() {
 		splash = new JFrame();
 		ImageIcon spl =
-			new ImageIcon(App.class.getResource("/ui/splash.png"));
+			new ImageIcon(App.class.getResource("/ui/splash.jpg"));
 		JLabel l = new JLabel();
 		l.setSize(400, 300);
 		l.setIcon(spl);
