2c6559a5005bef9413b8db20475c0ef9327cef29
advas14 <advas14@gmail.com>
2018-02-07 20:16:05 -0700

    refactored package

---
+++ src/testing/memoranda/testCalendar.java
@@ -0,0 +1,14 @@
+package testing.memoranda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCalendar {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
