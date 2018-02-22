089f9339091920e88937ca55171cad8dcdbf97f7
wjd345 <wjdavis1@asu.edu>
2018-02-22 15:41:56 -0700

    Had t omove test cases and rename test case package for gradle to work

---
+++ src/test/java/CalendarDateTest.java
@@ -0,0 +1,55 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Calendar;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.memoranda.date.CalendarDate;
+
+/* Tresor Cyubahiro 02.07.2018
+ * Add Unit Tests for CalendarDate class 
+ */
+class CalendarDateTest {
+	CalendarDate date = new CalendarDate();
+	/*
+	 * Test whether the date of instantiation is the same as today's date.
+	 */
+	@Test
+	public void todayDate() {
+		Calendar cal = Calendar.getInstance();
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int month = cal.get(Calendar.MONTH);
+		int year = cal.get(Calendar.YEAR);
+				
+		assertTrue(date.getDay() == day);
+		assertTrue(date.getMonth() == month);
+		assertTrue(date.getYear() == year);
+	}
+	
+	@Test
+	public void YesterdayTest() {
+		CalendarDate testYesterday = CalendarDate.yesterday();
+		
+		Calendar yesterday = Calendar.getInstance();
+		
+		yesterday.roll(Calendar.DATE, false);
+		
+		assertEquals(testYesterday.getDate().getTime(), yesterday.getTimeInMillis());
+	}
+	
+	@Test
+	/*
+	 * Test Tomorrow Method
+	 */
+	public void TomorrowTest() {
+		CalendarDate testTomorrow = CalendarDate.tomorrow();
+		
+		Calendar tomorrow = Calendar.getInstance();
+		tomorrow.roll(Calendar.DATE, true);
+		
+		assertEquals(testTomorrow.getDate().getTime(), tomorrow.getTimeInMillis());
+	}
+
+}
---
+++ src/test/java/ProjectImplTest.java
@@ -0,0 +1,48 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import main.java.memoranda.ProjectImpl;
+import nu.xom.Element;
+
+public class ProjectImplTest {
+
+	ProjectImpl project = new ProjectImpl(new Element("root_element"));
+		
+	@Test
+	/**
+	 * Test confirms public project._root elements are null
+	 */
+	public void TestNullAttributes() {
+		assertNull(project.getDescription());
+		assertNull(project.getStartDate());
+		assertNull(project.getEndDate());
+	}
+	
+	@Test
+	/**
+	 * Test confirms functionality of setTitle(String title)
+	 */
+	public void TestSetTitle() {
+		
+		assertNotNull(project.getTitle()); //Default title attribute is set to <> instead of null jsabbath 2/7/2018
+		
+		project.setTitle("project_title");
+		assertEquals(project.getTitle(), "project_title");	
+		assertFalse(project.getTitle().equals("wrong_title"));
+	}
+	
+	@Test
+	/**
+	 * Test confirms functionality of setDescription(String description)
+	 */
+	public void TestSetDescription() {
+		project.setDescription("a_description");
+		assertEquals(project.getDescription(), "a_description");
+		assertFalse(project.getDescription().equals("wront_description"));
+	}
+	
+	public void TestGetStatus() {
+		System.out.println(project.getStatus());
+	}
+}
