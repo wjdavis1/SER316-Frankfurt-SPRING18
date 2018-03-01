29b640116f91ba277b1d4814fc0f45925a3af82e
eotetb <jortner1@asu.edu>
2018-02-28 17:19:53 -0700

    US#28-Task46 added new methods

--- src/main/java/memoranda/RouteCollection.java
+++ src/main/java/memoranda/RouteCollection.java
@@ -11,10 +11,12 @@ package main.java.memoranda;
 
 
 import java.util.Map;
+import java.util.Set;
 import java.util.HashMap;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.File;
@@ -31,15 +33,18 @@ import org.json.*;
 public class RouteCollection implements JSONString, Serializable{
 	
 	private Map<String,RouteImpl> routeCollection;
+	private Set<String> routeIds;
 	
 	private String routeDataPath = "/data/routes/";
 	private File routesFile;
+	private FileInputStream in;
+	private String fileName;
 	
 	public RouteCollection(String fileName) {
 		routeDataPath += fileName;
-		System.out.println("[DEBUG]" +routeDataPath);
+		System.out.println("[DEBUG]" + routeDataPath);
 		routeCollection = new HashMap<>();
-		routesFile = new File(main.java.memoranda.ui.AppFrame.class.getResource(routeDataPath).getFile());
+		this.fileName = fileName;
 		if(routesFile.exists() && !routesFile.isDirectory()) {
 			System.out.println("File Exists!");
 		}
@@ -55,28 +60,162 @@ public class RouteCollection implements JSONString, Serializable{
 		}
 	}
 	
+	public boolean doesRouteExist(String routeId) {
+	    boolean doesExist = false;
+	    if (routeCollection.containsKey(routeId)) {
+	        doesExist = true;
+	    }
+	    return doesExist;
+	}
+	
 	public RouteCollection(JSONObject obj) {
 		
 	}
-	
+
+	/**
+     * Method: addRoute
+     * 
+     * Input: Route Object 
+     * Return: Boolean
+     * Description: Adds a new route to the RouteCollection HashMap, checks to see
+     * if the route is already in the set by route id. If the route is not in the system,
+     * the method adds it in and returns true, else false. The boolean return is for GUI
+     * purposes
+     */
 	public boolean addRoute(RouteImpl newRoute) {
-		
-		for(String routeId : routeCollection.keySet()) {
+		    boolean canAdd = false;
 			
-			if(!newRoute.getRouteId().equals(routeId)) {
+			if(!routeCollection.containsKey(newRoute.getRouteId())) {
 				routeCollection.put(newRoute.getRouteId(), newRoute);
-				return true;
+				canAdd = true;
 			}
-		}
-		return false;
-		
-	}
-
-	@Override
-	public String toJSONString() {
-		// TODO Auto-generated method stub
-		return null;
+		return canAdd;		
 	}
 	
+	/**
+     * Method: edit Route
+     * Input: Route ID, Edit - Information to be edited, 
+     * Option - JOptionPane integer value.
+     * Return boolean - determines if the edit can be done and sent to GUI
+     * 
+     * Description: Looks through the route collection to find the route
+     * based off of ID, then if the route is in the system, the edit
+     * of information based of the option integer can be made. The option
+     * integer is selected from the JOptionPane in the EditRoutePanel
+     */
+    public boolean editRoute(String routeId, String edit,int option) {
+        boolean canEdit = false;
+        
+        if(routeCollection.containsKey(routeId)) {
+            if (option == 0) {
+                routeCollection.get(routeId).setDestination(edit);
+            } 
+            else if ( option == 1) {
+                routeCollection.get(routeId).setRouteId(edit);
+            } 
+            else if (option == 2) {
+                routeCollection.get(routeId).setStartPoint(edit);
+            }
+            
+            canEdit = true;
+        }
+        
+        return canEdit;
+    }
+    
+    /**
+     * Method saveToFile
+     * 
+     * Input: None
+     * Return: None
+     * @throws IOException
+     * Description: Converts the RouteCollection into a string and writes it to the JSONFile
+     */
+    public void saveToFile() throws IOException {
+        FileWriter out = new FileWriter(routesFile);
+        JSONObject obj = new JSONObject();
+        for(String routeId : routeCollection.keySet()) {
+            obj.put(routeId, routeCollection.get(routeId).toJsonObject());
+        }
+        out.write(obj.toString());
+        out.flush();
+        out.close();
+    }
+    
+    /**
+     * Method: readFromFile
+     * Input: None
+     * Return: None
+     * Description: Reads in the JSON Objects from the file and converts them into the RouteCollection
+     */
+    public void readFromFile() {
+        routesFile = new File(main.java.memoranda.ui.AppFrame.class.getResource(routeDataPath).getFile());
+        if(routesFile.exists() && !routesFile.isDirectory()) {
+            System.out.println("[DEBUG] File Exists!");
+            try {
+                in = new FileInputStream(routesFile);
+                JSONObject obj = new JSONObject(new JSONTokener(in));
+                
+                if(obj.length() == 0) {
+                    
+                    System.out.println("[DEBUG] No Routes Available");
 
+                }else {
+                    String[] routeIds = JSONObject.getNames(obj);
+                    for(int i = 0; i < routeIds.length; i++) {
+                        System.out.println("[DEBUG] " + routeIds[i]);
+                        Route route = new RouteImpl((JSONObject)obj.getJSONObject(routeIds[i]));
+                        routeCollection.put(routeIds[i], route);
+                    }
+                }
+                
+                in.close();
+                
+                for(String routeId: routeCollection.keySet()) {
+                    System.out.println("[DEBUG] " + routeIds + " Route ID: " + routeCollection.get(routeIds).getRouteId()); 
+                }
+            }catch(IOException ioe) {
+                System.out.println(ioe.getMessage());
+            }
+        }else {
+            routesFile = new File(this.routeDataPath + fileName);
+            try {
+                if(routesFile.createNewFile()) {
+                    System.out.println("Created: " + fileName + " Found in: " + routeDataPath);
+                }
+            }catch(IOException ioe) {
+                System.out.println(ioe.getMessage());
+            }
+        }
+        
+    }
+    
+    /**
+     * Method: getRoutes
+     * Input: None
+     * Return: HashMap with route ids and information
+     * Description: Static method that creates a new HashMap Routes to be used with other panels
+     * and other classes for data retrieval.
+     */
+    public static HashMap<String,Route> getRoutes(){
+        Map<String, Route> routes = new HashMap<String, Route>();
+        String filePath = "/data/routes/routes.json";
+        File file = new File(main.java.memoranda.ui.AppFrame.class.getResource(filePath).getFile());
+        
+        try {
+            FileInputStream in = new FileInputStream(file);
+            JSONObject obj = new JSONObject(new JSONTokener(in));
+            
+            String[] ids = JSONObject.getRouteId(obj);
+            for(int i = 0; i < ids.length; i++) {
+                Route newRoute = new RouteImpl((JSONObject)obj.getJSONObject(ids[i]));
+                routes.put(ids[i], newRoute);
+            }
+            
+            in.close();
+        }catch(IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+        return (HashMap<String,Route>)routes;
+    }
 }
